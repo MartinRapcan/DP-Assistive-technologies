4fa17eca9cd06eb571c8faa67dd8fbe1cd88f889
Martin Rapčan <rapcan.rmartin@gmail.com>
2025-04-07 16:25:03 +0200

    feat: rework neonXR to work with multiple scenes

--- Assets/NeonXRI/Scripts/XRRayInteractorBridge.cs
+++ Assets/NeonXRI/Scripts/XRRayInteractorBridge.cs
@@ -8,14 +8,38 @@ public class XRRayInteractorBridge : MonoBehaviour
     private XRRayInteractor target;
     [SerializeField]
     private Transform reference;
-
+    [SerializeField]
+    private bool registerOnEnable = true;
+    
     public void OnGazeDataReady(GazeDataProvider gazeDataProvider)
     {
         if (reference == null)
         {
             reference = Camera.main.transform;
         }
+
+        if (target == null)
+        {    
+            target = reference.GetComponentInChildren<XRRayInteractor>();
+        }
+        
         target.transform.position = reference.TransformPoint(gazeDataProvider.GazeRay.origin);
         target.transform.forward = reference.TransformDirection(gazeDataProvider.GazeRay.direction);
+    }
+    
+    protected virtual void OnEnable()
+    {
+        if (registerOnEnable)
+        {
+            ServiceLocator.Instance.GazeDataProvider.gazeDataReady.AddListener(OnGazeDataReady);
+        }
+    }
+
+    protected virtual void OnDisable()
+    {
+        if (registerOnEnable)
+        {
+            ServiceLocator.Instance.GazeDataProvider.gazeDataReady.RemoveListener(OnGazeDataReady);
+        }
     }
 }
